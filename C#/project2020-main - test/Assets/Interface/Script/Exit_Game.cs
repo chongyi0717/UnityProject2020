bf2a38e818e84a5e7a2eb5bcf85c6eb2ab31515c
chongyi <chongyi.chong72@gmail.com>
2020-12-14 03:23:21 +0800

    combine

---
+++ C#/project2020-main - test/Assets/Interface/Script/Exit_Game.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Exit_Game : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Application.Quit();
+        GUILayout.EndHorizontal();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
